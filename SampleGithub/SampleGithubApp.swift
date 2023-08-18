32bd251374fd0805722c77da4a80805e406c93aa
zunema <yxxa.abc@gmail.com>
2023-08-19 00:54:36 +0900

    Initial Commit

---
+++ SampleGithub/SampleGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGithubApp.swift
+//  SampleGithub
+//
+//  Created by 飯尾悠也 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
